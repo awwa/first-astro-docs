b27f1bf494180e0c149b110e400cfbb7652410fe
Wataru Sato <awwa500@gmail.com>
2026-09-15 23:33:39 +0900

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   site: "https://first-astro-docs.awwa500.workers.dev",
+  integrations: [preact()],
 });
